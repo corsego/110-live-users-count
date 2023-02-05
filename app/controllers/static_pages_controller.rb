238ab48dd8871b20c30387fd1c64d362992560d8
yshmarov <yshmarov@gmail.com>
2023-02-05 23:28:06 +0100

    realtime track users online

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,12 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[ landing_page ]
+  skip_before_action :authenticate_user!, only: %i[ landing_page dashboard ]
 
   def landing_page
   end
 
   def dashboard
+    # list online users
+    users_online = Kredis.unique_list "users_online"
+    @users = User.find(users_online.elements)
   end
 end
